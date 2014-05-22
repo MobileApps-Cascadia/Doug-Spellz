b033d666f725d79425d7cbee235fbec63d667bf3
digid6 <digid6@gmail.com>
2014-05-22 15:36:13 -0700

    iOS Version
    Edit 1 missing third screen

---
+++ Spellz-Game/Design-phase1/Spellz/LMAFirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  LMAFirstViewController.m
+//  Spellz
+//
+//  Created by Doug Anderson on 5/21/14.
+//  Copyright (c) 2014 Doug Anderson Inc. All rights reserved.
+//
+
+#import "LMAFirstViewController.h"
+
+@interface LMAFirstViewController ()
+
+@end
+
+@implementation LMAFirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Spellz-Game/Design-phase1/Spellz/LMASecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  LMASecondViewController.m
+//  Spellz
+//
+//  Created by Doug Anderson on 5/21/14.
+//  Copyright (c) 2014 Doug Anderson Inc. All rights reserved.
+//
+
+#import "LMASecondViewController.h"
+
+@interface LMASecondViewController ()
+
+@end
+
+@implementation LMASecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
