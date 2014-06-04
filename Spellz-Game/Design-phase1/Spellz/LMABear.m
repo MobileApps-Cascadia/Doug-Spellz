15656a4c21ef921940dac6320b5dd0b761b0e165
NikkiBuck <nicolebuck@cc1-230-103.student.cascadia.edu>
2014-06-04 15:51:22 -0700

    navigation

---
+++ Spellz-Game/Design-phase1/Spellz/LMABear.m
@@ -0,0 +1,38 @@
+//
+//  LMABear.m
+//  Spellz
+//
+//  Created by Buck, Nicole D. on 6/4/14.
+//  Copyright (c) 2014 Doug Anderson Inc. All rights reserved.
+//
+
+#import "LMABear.h"
+
+@interface LMABear ()
+
+@end
+
+@implementation LMABear
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
